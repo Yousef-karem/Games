d717398dfe05006cd45bce136310a3ba161f4ddb
juliana <121284395+julianageorge@users.noreply.github.com>
2023-12-16 22:55:54 +0200

    Update Tic_Tac_Toc_App.cpp

--- Tic_Tac_Toc_App.cpp
+++ Tic_Tac_Toc_App.cpp
@@ -8,7 +8,7 @@ int main() {
     Player* players[2];
     players[0] = new Player (1, 'x');
 
-    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Welcome to FCAI Tic_Tac_Toc Game. :)\n";
     cout << "Press 1 if you want to play with computer and any number except 1 for another player : ";
     cin >> choice;
     if (choice != 1)
@@ -17,7 +17,7 @@ int main() {
         //Player pointer points to child
         players[1] = new RandomPlayer ('o', 5);
 
-    GameManager x_o_game (new Tic_Tac_Toe_Board(), players);
+    GameManager_Tic_Tac_Toc x_o_game (new Tic_Tac_Toe_Board(), players);
     x_o_game.run();
     system ("pause");
 }
